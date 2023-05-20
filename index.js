c182edcb1bdef269439177c7a07c6d4843cdac0c
saikumarpb <pkumar@securityinnovation.com>
2023-05-20 20:56:05 +0530

    Add user signup

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 app.use(express.json());
 
-const admin =   {
-    email: "admin@crms.com",
-    password: "admin"
-}
+const admin = {
+    email: 'admin@crms.com',
+    password: 'admin',
+};
 
-const token = "SuperSecretToken";
+const token = 'SuperSecretToken';
 
-const users = []
+const users = [];
 
-app.post("/admin/login",(req,res)=>{
-    const {email, password} = req.body
+app.post('/admin/login', (req, res) => {
+    const { email, password } = req.body;
 
-    if(!email || !password){
-        res.status(404)
-        res.send("Bad Request")
+    if (!email || !password) {
+        res.status(404);
+        res.send('Bad Request');
     }
 
-    if((email === admin.email) && (password===admin.password)){
+    if (email === admin.email && password === admin.password) {
         res.json({
-            token: token
-        })
+            token: token,
+        });
+    } else {
+        res.status(401);
+        res.send('Unauthorized');
+    }
+});
+
+app.post('/signup', (req, res) => {
+    const { email, password } = req.body;
+
+    // Sanity check
+    if (!email || !password) {
+        res.status(404);
+        res.send('Bad Request');
     }
 
-    res.status(401)
-    res.send("Unauthorized")
-})
+    // Existing user check
+    const existingUser = users.filter((user) => {
+        return user.email === email;
+    });
+
+    if (existingUser.length > 0) {
+        res.status(404);
+        res.send('Email already registered');
+    } else {
+        users.push({ email: email, password: password });
+        console.log(users);
+
+        res.send('User registered successfully');
+    }
+});
 
 app.get('/', (req, res) => {
-  res.send('Welcome to CRMS')
-})
+    res.send('Welcome to CRMS');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
